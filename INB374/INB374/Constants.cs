a4957e9f4fd96761a9753f40c48c6998e4be9162
Connor Livsey <connor.livsey@student.qut.edu.au>
2013-10-18 21:45:22 +1000

    Back to the old endpoints. Fixed ridiculous issue.
    The cache was only updated for the GET methods when an update we made
    with a PUT or POST. This limits the behaviour to just one web service.
    Turned that shit off.

--- INB374/INB374/Constants.cs
+++ INB374/INB374/Constants.cs
@@ -14,11 +14,11 @@ namespace INB374
     class Constants
     {
         #if PRD
-        public const string CUSTOMER_ENDPOINT = "http://fastws.qut.edu.au:8080/n8510873CustomerDB5823737525019890638/webresources/entities.customers";
-        public const string CUSTOMER_COUNT_ENDPOINT = "http://fastws.qut.edu.au:8080/n8510873CustomerDB5823737525019890638/webresources/entities.customers/count";
-        public const string PRODUCT_ENDPOINT = "http://fastws.qut.edu.au:8080/n8510873CustomerDB5823737525019890638/webresources/entities.products";
-        public const string ORDER_ENDPOINT = "http://fastws.qut.edu.au:8080/n8510873CustomerDB5823737525019890638/webresources/entities.orders";
-        public const string ORDERDETAILS_ENDPOINT = "https://fastws.qut.edu.au:8080/n8510873CustomerDB5823737525019890638/webresources/entities.orderdetails";
+        public const string CUSTOMER_ENDPOINT = "http://fastws.qut.edu.au:8080/n8510873CustomerDB/webresources/entities.customers";
+        public const string CUSTOMER_COUNT_ENDPOINT = "http://fastws.qut.edu.au:8080/n8510873CustomerDB/webresources/entities.customers/count";
+        public const string PRODUCT_ENDPOINT = "http://fastws.qut.edu.au:8080/n8510873CustomerDB/webresources/entities.products";
+        public const string ORDER_ENDPOINT = "http://fastws.qut.edu.au:8080/n8510873CustomerDB/webresources/entities.orders";
+        public const string ORDERDETAILS_ENDPOINT = "http://fastws.qut.edu.au:8080/n8510873CustomerDB/webresources/entities.orderdetails";
         #endif
         #if DEBUG
             public const string CUSTOMER_ENDPOINT = "http://localhost:8080/n8510873CustomerDB/webresources/entities.customers/"; 
